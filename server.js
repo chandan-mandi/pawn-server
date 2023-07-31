dd22d91ce2c256b13ddc6381e7add73645c57e92
Chandan Mandi <itscoderchandan@gmail.com>
2023-07-31 23:06:15 +0530

    without color

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const dotenv = require("dotenv").config();
-const colors = require("colors");
 
 const app = require("./app");
 const dbConnect = require("./utils/dbConnect");
@@ -11,5 +10,5 @@ const port = process.env.PORT || 8080;
 // Server
 
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`.bgCyan.bold);
+  console.log(`App is running on port ${port}`);
 });
